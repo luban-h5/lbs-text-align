63b4a8226bb35f2413e60a569d85bb166b2330ef
ly525 <lantern.done@gmail.com>
2019-10-27 23:32:51 +0800

    create text align plugin

--- src/main.js
+++ src/main.js
@@ -1,8 +1,64 @@
+/**
+ * #!zh: 鲁班支持组件：文字对齐组件
+ * #!en: Component that support Luban: for text align
+ */
 import Vue from 'vue'
-import App from './App.vue'
 
-Vue.config.productionTip = false
+Vue.config.productionTip = true
 
-new Vue({
-  render: h => h(App)
-}).$mount('#app')
+const Component = {
+  name: 'lbp-text-align',
+  render (h) {
+    return (
+      <div class="wrap">
+        <a-radio-group value={this.value} onChange={value => {
+          this.$emit('change', value)
+          this.$emit('input', value)
+        }} size="small">
+          {
+            this.textAlignTabs.map(item => (
+              <a-tooltip effect="dark" placement="top" key={item.value} title={item.label}>
+                <a-radio-button value={item.value}>
+                  <i class={['fa', 'fa-align-' + item.value]} aria-hidden="true"></i>
+                </a-radio-button>
+              </a-tooltip>
+            ))
+          }
+        </a-radio-group>
+      </div>
+    )
+  },
+  props: {
+    value: {
+      type: [String, Number]
+    }
+  },
+  data: () => ({
+    textAlignTabs: [{
+      label: '左对齐',
+      value: 'left'
+    },
+    {
+      label: '居中对齐',
+      value: 'center'
+    },
+    {
+      label: '右对齐',
+      value: 'right'
+    }]
+  })
+}
+
+const install = function (Vue) {
+  Vue.component(Component.name, Component)
+}
+
+// auto install
+if (typeof window !== 'undefined' && window.Vue) {
+  install(window.Vue)
+}
+
+export default {
+  install,
+  Component
+}
